e8f8da9a203f3ea0dbd66e8355966c74a82c3877
JoaoSerafim3001 <seafim.joao@gmail.com>
2022-12-01 21:00:34 +0000

    Poduct get by slug

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -12,29 +12,29 @@ router.get(
   })
 );
 
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    console.log("PRODUCT ID:", req.params.id);
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404).send({ message: "Product Not Found" });
-    }
-  })
-);
+// router.get(
+//   "/:id",
+//   asyncHandler(async (req, res) => {
+//     console.log("PRODUCT ID:", req.params.id);
+//     const product = await Product.findById(req.params.id);
+//     if (product) {
+//       res.json(product);
+//     } else {
+//       res.status(404).send({ message: "Product Not Found" });
+//     }
+//   })
+// );
 
-// *** GETTING AN ERROR ON THIS ROUTE
+// // *** GETTING AN ERROR ON THIS ROUTE
 
-// router.get("/:slug", async (req, res) => {
-//   const product = await Product.findOne({ slug: req.params.slug });
-//   if (product) {
-//     res.send(product);
-//   } else {
-//     res.status(404).send({ message: "Product Not Found" });
-//   }
-// });
+router.get("/:slug", async (req, res) => {
+  const product = await Product.findOne({ slug: req.params.slug });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
 
 router.post("/", async (req, res) => {
   try {
