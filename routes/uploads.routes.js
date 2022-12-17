e902dc164305b529c4176d9994ae0f97f817504e
JoaoSerafim3001 <seafim.joao@gmail.com>
2022-12-17 12:30:03 +0000

    Remove Tmp file

--- routes/uploads.routes.js
+++ routes/uploads.routes.js
@@ -1,11 +1,18 @@
 import dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs-extra";
 import { Router } from "express";
 import Category from "../models/Category.model.js";
 import asyncHandler from "express-async-handler";
-import { v2 as cloudinary } from "cloudinary";
 import { isLoggedIn, isAdmin } from "../middleware/auth.js";
 
 const router = Router();
+
+const removeTmpFile = (path) => {
+  fs.remove(path, (err) => {
+    if (err) throw err;
+  });
+};
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -25,6 +32,7 @@ router.post(
 
       const file = req.files.file;
       if (file.size > 1920 * 1920) {
+        removeTmpFile(file.tempFilePath);
         return res.status(400).json({ message: "Image size too large" });
       }
 
@@ -33,21 +41,20 @@ router.post(
         file.mimetype !== "image/png" &&
         file.mimetype !== "image/webp"
       ) {
+        removeTmpFile(file.tempFilePath);
+
         return res.status(400).json({ message: "Incorrect file format." });
       }
 
-      cloudinary.uploader.upload(
-        file.tempFilePath,
-        { folder: "eCommerce" },
-        (err, result) => {
-          res.json({
-            public_id: result.public_id,
-            secure_url: result.secure_url,
-          });
-          console.log();
-          console.log("**** File successfully uploaded!! ***");
-        }
-      );
+      cloudinary.uploader.upload(file.tempFilePath, { folder: "eCommerce" }, (err, result) => {
+        if (err) throw err;
+
+        removeTmpFile(file.tempFilePath);
+
+        res.json({ public_id: result.public_id, secure_url: result.secure_url });
+        console.log();
+        console.log("**** File successfully uploaded!! ***");
+      });
     } catch (err) {
       res.status(500).json(err);
     }
