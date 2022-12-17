1e0ec8afa660e8e6c6dcecbdb1b2de4d5ce7c53d
JoaoSerafim3001 <seafim.joao@gmail.com>
2022-12-17 11:31:24 +0000

    Fixed user.route

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ export const isLoggedIn = (req, res, next) => {
   if (req.user) {
     next();
   } else {
-    res.status(401).json({ message: "Please login first" }).redirect("/login");
+    res.status(401).json({ message: "Please login first" });
   }
 };
 
@@ -10,9 +10,6 @@ export const isAdmin = (req, res, next) => {
   if (req.user.isAdmin) {
     next();
   } else {
-    res
-      .status(401)
-      .json({ message: "You don't have permission" })
-      .redirect("/");
+    res.status(401).json({ message: "You don't have permission" });
   }
 };
