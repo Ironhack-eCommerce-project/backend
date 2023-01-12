45b2a9daab168e0d1760f20256062cd1986809fb
JoaoSerafim3001 <seafim.joao@gmail.com>
2023-01-12 22:42:54 +0000

    setHeaders

--- app.js
+++ app.js
@@ -44,6 +44,13 @@ app.use(
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
   })
 );
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
